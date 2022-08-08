83054fd09a0f94e0ad87dbec8573c01585cc4dd1
FilipeGitHub <filipw255255@gmail.com>
2022-08-08 15:20:50 +0200

    New 'design' page added, responsible for creating new taco

---
+++ src/main/java/com/tacos/TacoCloudApplication/Data/Taco.java
@@ -0,0 +1,13 @@
+package com.tacos.TacoCloudApplication.Data;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+
+}
